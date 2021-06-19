92b65aaa4a83f1e391a70573075d4800532073e0
florian schweizer <florian.schweizer@tum.de>
2021-06-19 08:07:50 +0200

    Initial Commit

---
+++ APIService/APIServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIServiceApp.swift
+//  APIService
+//
+//  Created by florian schweizer on 19.06.21.
+//
+
+import SwiftUI
+
+@main
+struct APIServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
